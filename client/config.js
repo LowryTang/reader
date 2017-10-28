ead4fabbc8d89f5c44f199c389dcdb62916fb7a8
Lowry Tang <lowrytang@gmail.com>
2017-10-27 22:17:36 -0700

    restart

--- client/config.js
+++
@@ -1,9 +0,0 @@
-let config = {
-  dev_host: 'http://localhost:3000',
-  prod_host: 'http://localhost:3000',
-  book_endpoint: '/api/v1',
-  chapter_endpoint: '/api/v1',
-  image_endpoint: '/api/v1/images',
-};
-
-module.exports = config;
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -6,11 +6,6 @@ var http = require('http')
 var https = require('https')
 var url = require('url')
 
-//
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({})
-
 var request = axios.create({
   baseURL: 'http://api.zhuishushenqi.com',
   headers: {
@@ -24,24 +19,7 @@ var request = axios.create({
 var controller = {
   searchBooks (req, res, next) {
     var params = req.query
-    // var options = {
-    //   uri: 'http://api.zhuishushenqi.com/book/fuzzy-search',
-    //   qs: { query },
-    //   method:"GET",
-    //   encoding: null,
-    //   headers: {
-    //     'X-Device-Id': '631cf212b409f949264fad9ba1ba1daa',
-    //     'X-User-Agent': 'YouShaQi/2.24.24',
-    //     'User-Agent': 'YouShaQi/2.24.24',
-    //     'Content-Type': 'application/json'
-    //   }
-    // };
-    // debug('search keyword is %s.', query, options);
-    // rq(options, function(err, response, body) {
-    //   debug("down", typeof body);
-    //   res.send(body);
-    // });
-
+    params.v = 1
     request.get('/book/fuzzy-search', { params }).then(result => {
       debug('finish api call.')
       res.send(result.data)
